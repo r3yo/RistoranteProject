3bb086af9a74f0be98df7e0253b6b015e9711827
Eleonora <mountainshepsky@gmail.com>
2025-08-12 21:28:24 +0200

    Fixed typos

--- menu/models.py
+++ menu/models.py
@@ -3,6 +3,8 @@ from django.db import models
 # Create your models here.
 class Category(models.Model):
     name = models.CharField(max_length=100, unique=True, blank=False)
+    class Meta:
+        verbose_name_plural = "Categories"
 
 
 class Dish(models.Model):
--- menu/views.py
+++ menu/views.py
@@ -5,6 +5,6 @@ from .models import *
 # Create your views here.
 def menu_list(request):
     categories = Category.objects.prefetch_related("items").all()
-    templ = "/menu/menu_list.html"
+    templ = "menu/menu_list.html"
     ctx = {"categories" : categories}
     return render(request, template_name=templ, context=ctx)
