d91a2b2f9a873b60e813c6e7e26959dd78a051e8
Eleonora <mountainshepsky@gmail.com>
2025-08-12 15:22:00 +0200

    Project tests

--- ristorante/settings.py
+++ ristorante/settings.py
@@ -119,6 +119,8 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATICFILES_DIR = [os.path.join(BASE_DIR, "static")]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
